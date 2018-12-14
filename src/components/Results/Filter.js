1c67dc954f976b9798d557ff159663bbb00cc8fa
kurarameshsai <rameshsai@xsilica.com>
2018-12-14 11:53:26 +0530

    latest

--- src/components/Results/Filter.js
+++ src/components/Results/Filter.js
@@ -4,133 +4,133 @@ import './results.css';
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse,  Dropdown, DropdownToggle, DropdownMenu,  DropdownItem } from "mdbreact";
-import {Input, MDBCol, MDBContainer, MDBRow,Button, Card, CardBody, CardImage, CardTitle, CardText, Col, MDBBtn } from 'mdbreact';
+import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "mdbreact";
+import { Input, MDBCol, MDBContainer, MDBRow, Button, Card, CardBody, CardImage, CardTitle, CardText, Col, MDBBtn } from 'mdbreact';
 
 
 class Filter extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isGoing: true,
-        numberOfGuests: 2
-      };
-  
-      this.handleInputChange = this.handleInputChange.bind(this);
-    }
-  
-    handleInputChange(event) {
-      const target = event.target;
-      const value = target.type === 'checkbox' ? target.checked : target.value;
-      const name = target.name;
-  
-      this.setState({
-        [name]: value
-      });
-    }
-    render() {
-      return (
-        <div>
-          <div>Filter
-  </div>
-  <h6>Service Type</h6>
-   <form>
-          <label> 
-          <input name="Hotel"      type="checkbox"      checked={this.state.isGoing}   onChange={this.handleInputChange} />
-    Hotel
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h6>Filter Service Type</h6>
+<div class="form-check">
+    <input type="checkbox" class="form-check-input" id="materialUnchecked"/>
+    <label class="form-check-label" for="materialUnchecked">Material unchecked</label>
+</div>
+        <form>
+          <label>
+            <input name="Hotel" type="checkbox"  onChange={this.handleInputChange} />
+            Hotel
           </label>
         </form>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         Banquet Hall
+            <input name="isGoing" type="checkbox"  onChange={this.handleInputChange} />
+            Banquet Hall
           </label>
         </form>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         Resort
+            <input name="isGoing" type="checkbox" onChange={this.handleInputChange} />
+            Resort
           </label>
         </form>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         Convention Hall
+            <input name="isGoing" type="checkbox" onChange={this.handleInputChange} />
+            Convention Hall
           </label>
         </form>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         Function Hall
+            <input name="isGoing" type="checkbox" onChange={this.handleInputChange} />
+            Function Hall
           </label>
         </form>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         Decoration
+            <input name="isGoing" type="checkbox"  onChange={this.handleInputChange} />
+            Decoration
           </label>
         </form>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-        Photography
+            <input name="isGoing" type="checkbox" onChange={this.handleInputChange} />
+            Photography
           </label>
         </form>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         Catering
+            <input name="isGoing" type="checkbox" onChange={this.handleInputChange} />
+            Catering
           </label>
         </form>
         <hr></hr>
         <h6>Budget</h6>
         <div className="my-5">
-        <label htmlFor="customRange1">Example range</label>
-        <input type="range" className="custom-range" id="customRange1" />
-      </div>
-      <h6>Food Type</h6>
+          {/* <label htmlFor="customRange1">Example range</label> */}
+          <input type="range" className="custom-range" id="customRange1" />
+        </div>
+        <h6>Food Type</h6>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         Vegetarian
+            <input name="isGoing" type="checkbox" onChange={this.handleInputChange} />
+            Vegetarian
           </label>
         </form>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         Non-Vegetarian        </label>
+            <input name="isGoing" type="checkbox" onChange={this.handleInputChange} />
+            Non-Vegetarian        </label>
         </form>
         <hr></hr>
-        <h6> Aminuties</h6>
+        <h6> Amenities</h6>
         <form>
           <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         AC
-          </label>
-        </form>
-        <form>
-          <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         WIFI
-          </label>
-        </form>
-  
-    <form>
-          <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         TV
+            <input name="isGoing" type="checkbox" onChange={this.handleInputChange} />
+            AC
           </label>
         </form>
         <form>
-          <label>
-          <input name="isGoing"  type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-         Parking
-          </label>
+        <input name="isGoing" type="checkbox"  onChange={this.handleInputChange} />
+          <label> WIFI </label>
         </form>
-      
-     </div>
-      );
-    }
-  }
 
-  export default Filter;
+        <form>
+          <label>
+            <input name="isGoing" type="checkbox" onChange={this.handleInputChange} />
+             TV
+          </label>
+        </form>
+        <form>
+          <label>
+            <input name="isGoing" type="checkbox"  onChange={this.handleInputChange} />
+             Parking
+          </label>
+        </form>
+
+      </div>
+    );
+  }
+}
+
+export default Filter;
--- src/components/Results/Header.js
+++ src/components/Results/Header.js
@@ -4,58 +4,54 @@ import './results.css';
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse,  Dropdown, DropdownToggle, DropdownMenu,  DropdownItem } from "mdbreact";
-import {Input, MDBCol, MDBContainer, MDBRow,Button, Card, CardBody, CardImage, CardTitle, CardText, Col, MDBBtn } from 'mdbreact';
-
+import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "mdbreact";
+import { Input, MDBCol, MDBContainer, MDBRow, Button, Card, CardBody, CardImage, CardTitle, CardText, Col, MDBBtn } from 'mdbreact';
 
 class Header extends React.Component {
-    state = {
-      isOpen: false,
-    };
-  
-    toggleCollapse = this.setState({ isOpen: !this.state.isOpen });
-   
-    render() {
-       return (
-          <div>
-             <Navbar color="white" dark expand="md" style={{marginTop: "0px"}}>
-            <NavbarBrand>           
-               <img style={{height:"27px"}} src='http://design.maa-aahwanam.com/responsivedesign/img/logo.png'/>
-  
-              <strong className="black-text">Ahwanam</strong>
-            </NavbarBrand>
-            <NavbarToggler    onClick={this.toggleCollapse} />
-            <Collapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-              <NavbarNav left style={{marginLeft:"50%"}}>
-                <NavItem active>
-                  <NavLink to="#!"> <strong className="black-text">Login/Register</strong></NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink to="#!"> <strong className="black-text"><button type="button" className="btn btn-danger btn-sm waves-effect waves-light" style={{
-                    fontsize: "11px", fontweight: "1000"}}>Call: +91 7032199007</button></strong></NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink to="#!"> <img src='http://design.maa-aahwanam.com/responsivedesign/img/india.png'/></NavLink>
-                </NavItem>
-                <NavItem>
-                  <Dropdown>
-                    <DropdownToggle  nav caret>
-                      <div className="d-none d-md-inline"> <strong className="black-text">Dropdown</strong></div>
-                    </DropdownToggle>
-                    <DropdownMenu className="dropdown-default"right>
-                      <DropdownItem href="#!"> <strong className="black-text">Action</strong></DropdownItem>
-                      <DropdownItem href="#!"> <strong className="black-text">Another Action</strong></DropdownItem>
-                      <DropdownItem href="#!"> <strong className="black-text">Something else here</strong></DropdownItem>
-                      <DropdownItem href="#!"> <strong className="black-text">Something else here</strong></DropdownItem>
-                    </DropdownMenu>
-                  </Dropdown>
-                </NavItem>
-              </NavbarNav>
-              
-            </Collapse>
+  state = {
+    isOpen: false,
+  };
+  toggleCollapse = this.setState({ isOpen: !this.state.isOpen });
+  render() {
+    return (
+      <div>
+        <Navbar color="white" dark expand="md" style={{ marginTop: "0px" }}>
+          <NavbarBrand>
+            <img style={{ height: "27px" }} src='http://design.maa-aahwanam.com/responsivedesign/img/logo.png' />
+            <strong className="black-text">Ahwanam</strong>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggleCollapse} />
+          <Collapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+            <NavbarNav left style={{ marginLeft: "50%" }}>
+              <NavItem active>
+                <NavLink to="#!"> <strong className="black-text">Login/Register</strong></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#!"> <strong className="black-text"><button type="button" className="btn btn-danger btn-sm waves-effect waves-light" style={{
+                  fontsize: "11px", fontweight: "1000"
+                }}>Call: +91 7032199007</button></strong></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#!"> <img src='http://design.maa-aahwanam.com/responsivedesign/img/india.png' /></NavLink>
+              </NavItem>
+              <NavItem>
+                <Dropdown>
+                  <DropdownToggle nav caret>
+                    <div className="d-none d-md-inline"> <strong className="black-text">Dropdown</strong></div>
+                  </DropdownToggle>
+                  <DropdownMenu className="dropdown-default" right>
+                    <DropdownItem href="#!"> <strong className="black-text">Action</strong></DropdownItem>
+                    <DropdownItem href="#!"> <strong className="black-text">Another Action</strong></DropdownItem>
+                    <DropdownItem href="#!"> <strong className="black-text">Something else here</strong></DropdownItem>
+                    <DropdownItem href="#!"> <strong className="black-text">Something else here</strong></DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </NavItem>
+            </NavbarNav>
+          </Collapse>
         </Navbar>
-          </div>
-       );
-    }
+      </div>
+    );
   }
-  export default Header;
+}
+export default Header;
