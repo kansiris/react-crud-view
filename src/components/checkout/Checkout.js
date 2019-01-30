883f4610c94880a12797a3c29e16be663853cdc1
lakshmixsilica <lakshmi.p@xsilica.com>
2019-01-30 16:05:34 +0530

    latest

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -7,7 +7,7 @@ import Header from '../Results/Header'
 class Checkout extends Component {
     constructor(props) {
         super(props);
-        this.state = { checked: false,  payment_amount: 0, amount: 0,Firstname:'',Lastname:'',Email:'',Password:'',mobile1:'',OrderId:'',Orderdate:'',OrderStatus:'',Paymentid:'',CustomerId:'',ordertime:'',orderdeliveredtime:'',Discount:'',Remarks:'',Deliverycharges:'',CGST:'',SGST:'',Totalamount:'',Deliveryarea:'',Transactionid:'',transactionstatus:'' };
+        this.state = { checked: false,  payment_amount: 0, amount: 0,Firstname:'',Lastname:'',Email:'',Password:'',mobile1:'',OrderId:'',Orderdate:'',OrderStatus:'New order',Paymentid:'',CustomerId:'',ordertime:'',orderdeliveredtime:'',Discount:'',Remarks:'',Deliverycharges:'',CGST:'',SGST:'',Totalamount:'',Deliveryarea:'',Transactionid:'',transactionstatus:'' };
         this.handleChange = this.handleChange.bind(this);
             this.paymentHandler = this.paymentHandler.bind(this);
             this.changeAmount = this.changeAmount.bind(this);
@@ -87,16 +87,18 @@ class Checkout extends Component {
             //     console.error(error);
             //     alert('failed');
             //   }); 
-            fetch('http://localhost:64017/api/Order/placeorder?email='+localStorage.getItem('Email'),{
+            fetch('http://localhost:64017/api/Order/placeorder?email='+localStorage.getItem('Email')+'&&pid='+localStorage.getItem('cartno'),{
                 method: 'POST',
-                body:JSON.stringify({OrderId,Orderdate,OrderStatus,Paymentid:response.razorpay_payment_id,CustomerId,ordertime,orderdeliveredtime,Discount,Remarks,Deliverycharges,CGST,SGST,Totalamount,Deliveryarea,Transactionid:response.razorpay_payment_id,transactionstatus}),
+                body:JSON.stringify({OrderId:OrderId,Orderdate:Orderdate,OrderStatus:OrderStatus,Paymentid:response.razorpay_payment_id,CustomerId:CustomerId,ordertime:ordertime,Discount:Discount,Remarks:Remarks,Deliverycharges:Deliverycharges,CGST:CGST,SGST:SGST,Totalamount:Totalamount,Deliveryarea:Deliveryarea,Transactionid:response.razorpay_payment_id,transactionstatus:transactionstatus}),
                  headers: {
                   'Accept': 'application/json',
                   'Content-Type': 'application/json'
               }
             }).then((response) => response.json())
             .then((responseJson) => {
-                   this.setState({OrderId:'',Orderdate:'',OrderStatus:'',Paymentid:'',CustomerId:'',ordertime:'',orderdeliveredtime:'',Discount:'',Remarks:'',Deliverycharges:'',CGST:'',SGST:'',Totalamount:'',Deliveryarea:'',Transactionid:'',transactionstatus:''});
+                localStorage.setItem('Oid',responseJson);
+                alert(responseJson);
+                //    this.setState({OrderId:'',Orderdate:'',OrderStatus:'',Paymentid:'',CustomerId:'',ordertime:'',orderdeliveredtime:'',Discount:'',Remarks:'',Deliverycharges:'',CGST:'',SGST:'',Totalamount:'',Deliveryarea:'',Transactionid:'',transactionstatus:''});
                    return responseJson.success;      
             })
             .catch((error) => {
